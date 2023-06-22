4ff6970aba86b635aa75f0c6f2a31d54754737ab
WilkiMel <40223960+WilkiMel@users.noreply.github.com>
2023-06-22 17:44:00 -0500

    Add files via upload

---
+++ Englishworks/Movies/script.js
@@ -0,0 +1,13 @@
+let genre = parseInt(prompt("Tell me a genre and I will recommend a movie.\n Comedy = 1\n Terror = 2\n Drama = 3\n Action = 4"));
+
+if (genre == 1) {
+    alert("I recommend that you watch: 1. The Naked Gun / 2. Ghostbusters / 3. Me, Myself & Irene")
+} else if (genre == 2) {
+    alert("I recommend that you watch: 1. THE RING / 2. CANDYMAN / 3. LAND OF THE DEAD")
+} else if (genre == 3) {
+    alert("I recommend that you watch: 1. Gladiator / 2. Titanic / 3. Eternal Sunshine of the Spotless Mind")
+} else if (genre == 4) {
+    alert("I recommend that you watch: 1. Spiderman Homecoming / 2. Avengers: End Game / 3. John Wick 1")
+} else{
+    alert("I don't have any recommendations")
+} 
